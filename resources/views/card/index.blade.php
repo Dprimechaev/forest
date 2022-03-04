2dd239b6cc0ca67fb29607cee62a8b43f31813be
Dmitriy <dprimechaev@bk.ru>
2022-03-04 11:32:38 +0300

    first commit

---
+++ resources/views/card/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+@section('title', 'Карточки' )
+
+
+@section('content')
+
+@endsection
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.layout')
+@section('title', 'Главная' )
+
+@section('content')
+@endsection
